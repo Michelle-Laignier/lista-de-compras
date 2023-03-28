d172981f86483d7575662f5b8ed4ebac8f8de39f
Laignier <michellelaignier17@gmail.com>
2023-03-27 23:20:39 -0300

    itens ficam salvos no localStorage

--- js/main.js
+++ js/main.js
@@ -11,9 +11,10 @@ function removeLinha(event) {
     buttonRemoveClicked.parentElement.remove();
 }
 
+//////////////////////////////////////////////////////////////////////
 let buttonAdd = document.querySelector(".button-add")
-
 buttonAdd.addEventListener("click", adicionaLinha)
+buttonAdd.addEventListener("click", salvarNoLocalStorage)
 
 function adicionaLinha () {
 
@@ -21,8 +22,8 @@ function adicionaLinha () {
 
     let newDiv = document.createElement("div")
     let newInputCheck = document.createElement("input")
-    let newInputNumber = document.createElement("input") //(se não ficarem dentro da função
-    let newInputText = document.createElement("input") //as barras de input não aparecem)
+    let newInputNumber = document.createElement("p") //(se não ficarem dentro da função
+    let newInputText = document.createElement("p") //as barras de input não aparecem)
     let newButton = document.createElement("button")
 
     newInputCheck.type = "Checkbox"
@@ -35,17 +36,57 @@ function adicionaLinha () {
 
     newButton.textContent = "Remover"
 
-    lista.appendChild(newDiv)
-    newDiv.appendChild(newInputCheck)
-    newDiv.appendChild(newInputNumber)
-    newDiv.appendChild(newInputText)
-    newDiv.appendChild(newButton)
-
     newDiv.classList.add("lista-div")
     newInputCheck.classList.add("lista-check")
     newInputNumber.classList.add("lista-quantidade")
     newInputText.classList.add("lista-produto")
     newButton.classList.add("button-remove")
 
+    if (inputNumber.value == "" && inputText.value == "") {
+        alert("Preencha pelo menos um dos campos")
+    } else if (inputText.value == "") {
+        alert("Preencha o campo 'item'")
+    } else {
+        lista.appendChild(newDiv)
+        newDiv.appendChild(newInputCheck)
+        newDiv.appendChild(newInputNumber)
+        newDiv.appendChild(newInputText)
+        newDiv.appendChild(newButton)
+    }
+
     newButton.addEventListener("click", removeLinha)
+
+    //pra aparecer nos campos novos os valores digitados nos inputs
+    newInputNumber.textContent = inputNumber.value
+    newInputText.textContent = inputText.value
+}
+
+////////////////////////////////////////////////////////////////////////
+let inputNumber = document.getElementById("quantidade")
+let inputText = document.getElementById("produto")
+
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+function salvarNoLocalStorage () {
+    
+    let newInputNumber = document.querySelector(".lista-quantidade")
+    let newInputText = document.querySelector(".lista-produto")
+
+    newInputNumber.textContent = inputNumber.value
+    newInputText.textContent = inputText.value
+
+    const itemAtual = {
+        "item": newInputNumber.textContent,
+        "quantidade": newInputText.textContent
+    }
+
+    if (inputNumber.value != "" && inputText.value != "") {
+        itens.push(itemAtual)
+        localStorage.setItem("itens", JSON.stringify(itens))
+    } else if (inputNumber.value == "" && inputText.value != "") {
+        itens.push(itemAtual)
+        localStorage.setItem("itens", JSON.stringify(itens))
+    } else {
+        return
+    }
 }
